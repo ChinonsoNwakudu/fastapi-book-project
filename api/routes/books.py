4418f7dcfbd0ef7d53618a34ff0e03d7c3f06af0
Chinonso Nwakudu <nwakudue@gmail.com>
2025-02-11 20:44:17 +0300

    Added new endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -32,6 +32,15 @@ db.books = {
     ),
 }
 
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book_by_id(book_id: int):
+    book = db.books.get(book_id)
+    if not book:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"detail": "Book not found"},
+        )
+    return book
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(book: Book):
